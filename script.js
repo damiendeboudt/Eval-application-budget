59d11de8cdae9ffe5b7029efdc7b641659b85da4
Damien <damien.deboudt@sfr.fr>
2022-12-20 19:44:25 +0100

    OK avec boucles

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 let buttonCalculate = document.getElementById("Button-result");
+let buttonAddRecette = document.getElementById("ajouter-recette");
+let buttonAddDepenses  = document.getElementById('ajouter-depense');
+
+
+let depenses = document.getElementById("depense-fixe")
+let recette = document.getElementById("Recettes")
 
 let inputDepense = document.getElementById("depense-fixe").getElementsByTagName("input");
 let inputRecettes = document.getElementById("Recettes").getElementsByTagName("input");
@@ -6,39 +12,43 @@ let inputEpargne = document.getElementById("Epargne").getElementsByTagName("inpu
 
 let spanDepense = document.getElementById("depense");
 let spanRecette = document.getElementById("Recette");
+let spanBudget = document.getElementById("restant");
 
-buttonCalculate.addEventListener("click", ()=>{
-    //calcul des Depenses en incrementant les values des inputs Depenses
-    let depense = 0
-    for(let i =0; i<inputDepense.length; i ++) {
-        console.log(inputDepense[i].value)
-        depense += parseFloat(inputDepense[i].value)
-    }
-    spanDepense.innerText += depense
+//Ajout d'un input depense
+buttonAddDepenses.addEventListener("click", ()=>{
+    let AddInput = document.createElement("input")
+    AddInput.value = 0
+    depenses.appendChild(AddInput)
+})
 
-    //calcul des Recettes en incrementant les values des inputs Recettes
-    let recette = 0
-    for(let i =0; i<inputRecettes.length; i ++) {
-        console.log(inputRecettes[i].value)
-        depense += parseFloat(inputRecettes[i].value)
-    }
-    spanRecette.innerText += recette
+//Ajout d'un input Recettes
+buttonAddRecette.addEventListener("click", ()=>{
+    let AddInput = document.createElement("input")
+    AddInput.value = 0
+    recette.appendChild(AddInput)
 })
 
 
 
 
-
-/*
-
-buttonSubmit.addEventListener("click", ()=>{
+buttonCalculate.addEventListener("click", ()=>{
+    //calcul des Depenses en incrementant les values des inputs Depenses
     let depense = 0
-    for(let i =0; i<input.length; i ++) {
-        console.log(input[i].value)
-        //input[1].value = (input[1].value * 2)
-        depense += parseFloat(input[i].value)
+    for(let i =0; i<inputDepense.length; i ++) {
+        depense += parseFloat(inputDepense[i].value)
     }
+    spanDepense.innerText = "Depense: " + depense
 
-    console.log(depense)
-    divresult.innerText += depense
-})*/
+    //calcul des Recettes en incrementant les values des inputs Recettes
+    let recette = 0
+    for(let i =0; i<inputRecettes.length; i ++) {
+        recette += parseFloat(inputRecettes[i].value)
+    }
+    //calcul epargne en incrementant value epargne
+    let epargne = 0
+    for(let i =0; i<inputEpargne.length; i ++) {
+        epargne += parseFloat(inputEpargne[i].value)
+    }
+    spanRecette.innerText = "Recettes: " + recette
+    spanBudget.innerText = "Budget restant: " + ((recette + epargne) - depense)
+})
